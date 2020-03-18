af4e328ebff062f418c98a0fe3976ac414e69877
LIU JIAN <onanying@126.com>
2020-03-18 17:36:14 +0800

    fix: 修改路由重写

--- app/Web/Route/Router.php
+++ app/Web/Route/Router.php
@@ -14,9 +14,10 @@ class Router extends \Mix\Route\Router
 
     /**
      * 404 处理
+     * @param \Exception $exception
      * @param Response $response
      */
-    public static function show404(Response $response)
+    public function show404(\Exception $exception, Response $response)
     {
         $response = ResponseHelper::view($response, 'errors.not_found', [
             'message' => '404 Not Found',
@@ -26,18 +27,18 @@ class Router extends \Mix\Route\Router
 
     /**
      * 500 处理
-     * @param \Throwable $e
+     * @param \Exception $exception
      * @param Response $response
      */
-    public static function show500(\Throwable $e, Response $response)
+    public function show500(\Exception $exception, Response $response)
     {
         $response = ResponseHelper::view($response, 'errors.internal_server_error', [
-            'message' => $e->getMessage(),
-            'type'    => get_class($e),
-            'code'    => $e->getCode(),
-            'file'    => $e->getFile(),
-            'line'    => $e->getLine(),
-            'trace'   => $e->getTraceAsString(),
+            'message' => $exception->getMessage(),
+            'type'    => get_class($exception),
+            'code'    => $exception->getCode(),
+            'file'    => $exception->getFile(),
+            'line'    => $exception->getLine(),
+            'trace'   => $exception->getTraceAsString(),
         ]);
         $response->withStatus(500)->end();
     }
