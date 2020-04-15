2dbac6eaefedf39cf7d681b31d39d21edc233648
LIU JIAN <onanying@126.com>
2020-04-15 16:21:11 +0800

    feat: rpc server 默认随机端口

--- manifest/beans/grpc.php
+++ manifest/beans/grpc.php
@@ -17,13 +17,6 @@ return [
     [
         // 类路径
         'class'           => \Mix\Grpc\Server::class,
-        // 构造函数注入
-        'constructorArgs' => [
-            // host
-            '0.0.0.0',
-            // port
-            9506,
-        ],
         // 属性注入
         'properties'      => [
             // 事件调度器
